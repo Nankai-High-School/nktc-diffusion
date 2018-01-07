42cf16b11653479b7e7742302def2be82fe6f76f
lookas2001 <lookas2001@qq.com>
2018-01-07 14:17:18 +0800

    With Slogen Background

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,61 +1,70 @@
 <template>
   <main class="home">
-    <div class="slogen">
+    <div class="slogen fullscreen-slogen">
+      <canvas ref="background"></canvas>
       <div class="container">
         <h1>相信</h1>
         <h2>传承的力量</h2>
       </div>
     </div>
-    <div class="banner">
-      <div class="container">
-        <h3>2018</h3>
-        <h1>热传递</h1>
-        <h2>高校信息普及计划</h2>
-      </div>
-    </div>
-    <div class="university-list">
-      <div class="container">
-        <h2>我想了解</h2>
-        <ul>
-          <li><router-link :to="{ name: 'UniversityItem', params: { name: 'pku' }}">北京大学</router-link></li>
-          <li>清华大学</li>
-          <li>浙江大学</li>
-          <li>复旦大学</li>
-          <li>中国人民大学</li>
-          <li>上海交通大学</li>
-          <li>天津大学</li>
-          <li>南开大学</li>
-        </ul>
-        <div class="more">
-          <router-link :to="{ name: 'UniversityList'}">查看更多</router-link>
+
+    <div class="fullscreen-other">
+      <div class="banner">
+        <div class="container">
+          <h3>2018</h3>
+          <h1>热传递</h1>
+          <h2>高校信息普及计划</h2>
+          <h4><button>什么是热传递？</button></h4>
+          <!--<div class="intro">-->
+          <!--<div class="container">-->
+          <!--<h2>什么是热传递</h2>-->
+          <!--<p>如果提前感受梦想与现实的差距，也许我们能更好的前行。</p>-->
+          <!--<p>这次，我们把梦想变成信息流，然后把更多的梦想变成现实。</p>-->
+          <!--<p>热传递计划是一个邀请已毕业南开中学学长学姐，回校交流的活动。</p>-->
+          <!--<p>我们相信梦想的力量，更相信传承的力量。</p>-->
+          <!--<p>聚是一团火，化作漫天星。</p>-->
+          <!--</div>-->
+          <!--</div>-->
         </div>
       </div>
-    </div>
-    <div class="intro">
-      <div class="container">
-        <h2>什么是热传递</h2>
-        <p>如果提前感受梦想与现实的差距，也许我们能更好的前行。</p>
-        <p>这次，我们把梦想变成信息流，然后把更多的梦想变成现实。</p>
-        <p>热传递计划是一个邀请已毕业南开中学学长学姐，回校交流的活动。</p>
-        <p>我们相信梦想的力量，更相信传承的力量。</p>
-        <p>聚是一团火，化作漫天星。</p>
+
+
+      <div class="university-list">
+        <div class="container">
+          <h2>我想了解</h2>
+          <ul>
+            <li>
+              <router-link :to="{ name: 'UniversityItem', params: { name: 'pku' }}">北京大学</router-link>
+            </li>
+            <li>清华大学</li>
+            <li>浙江大学</li>
+            <li>复旦大学</li>
+            <li>中国人民大学</li>
+            <li>上海交通大学</li>
+            <li>天津大学</li>
+            <li>南开大学</li>
+          </ul>
+          <div class="more">
+            <router-link :to="{ name: 'UniversityList'}">查看更多</router-link>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="university-create">
-      <div class="container">
-        <h2>我想告诉</h2>
-        <p>如果你想要跟学弟学妹们分享自己的学校，欢迎发送邮件到 <a href="mailto:haiqin.cui@nktry.com">haiqin.cui@nktry.com</a> 跟我们联系。</p>
-        <p>如果您不方便使用邮箱，可以使用下方联系方式。</p>
-        <address>
-          Tel: <a href="tel:+8615822379150">+86 158 2237 9150</a> <br>
-          QQ: 1779161430 <br>
-          WeChat: lookas2001 <br>
-        </address>
+      <div class="university-create">
+        <div class="container">
+          <h2>我想告诉</h2>
+          <p>如果你想要跟学弟学妹们分享自己的学校，欢迎发送邮件到 <a href="mailto:haiqin.cui@nktry.com">haiqin.cui@nktry.com</a> 跟我们联系。</p>
+          <p>如果您不方便使用邮箱，可以使用下方联系方式。</p>
+          <address>
+            Tel: <a href="tel:+8615822379150">+86 158 2237 9150</a> <br>
+            QQ: 1779161430 <br>
+            WeChat: lookas2001 <br>
+          </address>
+        </div>
       </div>
+      <!--<app-share></app-share>-->
+      <!--&lt;!&ndash;我要宣传&ndash;&gt;-->
+      <!--<app-feedback></app-feedback>-->
     </div>
-    <app-share></app-share>
-    <!--我要宣传-->
-    <app-feedback></app-feedback>
   </main>
 </template>
 
@@ -70,17 +79,195 @@
         }
       }
     },
-    components: {}
+    methods: {
+      handleScroll () {
+        return 0
+      },
+      mountBackground () {
+        // https://segmentfault.com/a/1190000009675230
+        let el = this.$refs.background
+        let ctx = el.getContext('2d')
+        let mousePos = [0, 0]
+        let nodes = []
+        let edges = []
+
+        let backgroundColor = '#222' // 背景颜色
+        let nodeColor = '#999' // 点颜色
+        let edgeColor = '#999' // 边颜色
+
+        function step () { // 每帧重新绘制
+          nodes.forEach(function (e) {
+            if (e.drivenByMouse) {
+              return
+            }
+
+            // 更新位置
+            e.x += e.vx
+            e.y += e.vy
+
+            // 反弹
+            function clamp (min, max, value) {
+              if (value > max) {
+                return max
+              } else if (value < min) {
+                return min
+              } else {
+                return value
+              }
+            }
+
+            if (e.x <= 0 || e.x >= el.width) {
+              e.vx *= -1
+              e.x = clamp(0, el.width, e.x)
+            }
+
+            if (e.y <= 0 || e.y >= el.height) {
+              e.vy *= -1
+              e.y = clamp(0, el.height, e.y)
+            }
+          })
+
+          // 第一个点作为鼠标的跟随点
+          let easingFactor = 5.0  // 缓动因子
+          nodes[0].x += (mousePos[0] - nodes[0].x) / easingFactor
+          nodes[0].y += (mousePos[1] - nodes[0].y) / easingFactor
+
+          render()
+          window.requestAnimationFrame(step)
+        }
+
+        function render () {
+          // 绘制
+          ctx.fillStyle = backgroundColor
+          ctx.fillRect(0, 0, el.width, el.height)
+
+          let threshold = Math.sqrt(Math.pow((el.width), 2) + Math.pow((el.height), 2)) / 8
+          edges.forEach(function (e) {
+            let l = Math.sqrt(Math.pow((e.from.x - e.to.x), 2) + Math.pow((e.from.y - e.to.y), 2)) // 求边长
+
+            if (l > threshold) {
+              return
+            }
+
+            ctx.strokeStyle = edgeColor
+            ctx.lineWidth = (1.0 - l / threshold) * 2.5
+            ctx.globalAlpha = 1.0 - l / threshold
+            ctx.beginPath()
+            ctx.moveTo(e.from.x, e.from.y)
+            ctx.lineTo(e.to.x, e.to.y)
+            ctx.stroke()
+          })
+          ctx.globalAlpha = 1.0
+
+          nodes.forEach(function (e) {
+            if (e.drivenByMouse) {
+              return
+            }
+
+            ctx.fillStyle = nodeColor
+            ctx.beginPath()
+            ctx.arc(e.x, e.y, e.radius, 0, 2 * Math.PI)
+            ctx.fill()
+          })
+        }
+
+        // 创建画布
+        window.onresize = function () {
+          el.width = parseFloat(window.getComputedStyle(el).getPropertyValue('width'))
+          el.height = parseFloat(window.getComputedStyle(el).getPropertyValue('height'))
+
+          if (nodes.length === 0) {
+            // 创建点
+            let nodeNum = Math.sqrt(Math.pow((el.width), 2) + Math.pow((el.height), 2)) / 10
+            for (let i = 0; i < nodeNum; i++) {
+              let node = {
+                drivenByMouse: i === 0,
+                x: Math.random() * el.width,
+                y: Math.random() * el.height,
+                vx: Math.random() * 1 - 0.5, // 速度 -0.5~0.5
+                vy: Math.random() * 1 - 0.5,
+                radius: Math.random() > 0.9 ? 3 + Math.random() * 3 : 1 + Math.random() * 3 // 点的大小
+              }
+
+              nodes.push(node)
+            }
+
+            // 创建边
+            nodes.forEach(function (e) {
+              nodes.forEach(function (e2) {
+                if (e === e2) {
+                  return
+                }
+
+                let edge = {
+                  from: e,
+                  to: e2
+                }
+
+                let ignore = false
+
+                // 存在的边就不存了
+                edges.forEach(function (e) {
+                  if (e.from === edge.from & e.to === edge.to) {
+                    ignore = true
+                  }
+
+                  if (e.to === edge.from & e.from === edge.to) {
+                    ignore = true
+                  }
+                })
+
+                if (!ignore) {
+                  edges.push(edge)
+                }
+              })
+            })
+          }
+
+          render()
+        }
+
+        window.onresize()
+
+        window.onmousemove = function (e) {
+          mousePos[0] = e.clientX
+          mousePos[1] = e.clientY
+        }
+
+        window.requestAnimationFrame(step)
+      }
+    },
+    components: {},
+    mounted () {
+      this.mountBackground()
+    },
+    created () {
+      window.addEventListener('scroll', this.handleScroll)
+    },
+    destroyed () {
+      window.removeEventListener('scroll', this.handleScroll)
+    }
   }
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
   .home {
-    > .slogen {
+    color: #f8f9fa;
+    .slogen {
       height: 100vh;
-      background-color: #888;
+      background-color: #222;
+
+      > canvas {
+        position: absolute;
+        top: 0;
+        z-index: 1;
+        width: 100vw;
+        height: 100vh;
+      }
 
       > .container {
+        position: relative; // 使得z-index生效
+        z-index: 2;
         padding: 20vh 10vw 0;
 
         > h1 {
@@ -93,12 +280,17 @@
       }
     }
 
-    > .banner {
+    .banner {
       height: 90vh;
-      background-color: #aaa;
+      background-color: #333;
 
       > .container {
         padding: 15vh 10vw 0;
+
+        > h3 {
+          margin-bottom: 0;
+          font-size: 1.75rem;
+        }
 
         > h1 {
           font-size: 5rem;
@@ -108,15 +300,28 @@
           font-size: 2.5rem;
         }
 
-        > h3 {
-          margin-bottom: 0;
-          font-size: 1.75rem;
+        > h4 {
+          margin-top: 1rem;
+          font-size: 1.25rem;
         }
+
+
+        /*.intro {*/
+        /*background-color: #555;*/
+
+        /*> .container {*/
+        /*padding: 30px 20px;*/
+
+        /*> h2 {*/
+        /*font-size: 2.5rem;*/
+        /*}*/
+        /*}*/
+        /*}*/
       }
     }
 
-    > .university-list {
-      background-color: #ccc;
+    .university-list {
+      background-color: #444;
 
       > .container {
         padding: 30px 20px;
@@ -127,20 +332,8 @@
       }
     }
 
-    > .intro {
-      background-color: #ddd;
-
-      > .container {
-        padding: 30px 20px;
-
-        > h2 {
-          font-size: 2.5rem;
-        }
-      }
-    }
-
-    > .university-create {
-      background-color: #eee;
+    .university-create {
+      background-color: #555;
 
       > .container {
         padding: 30px 20px;
